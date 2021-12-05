09eb378ce20599470c4d701698c2008081843f62
ShaharYemini <76625369+ShaharYemini@users.noreply.github.com>
2021-12-05 02:45:05 +0200

    004
    שינוי קטן

--- increment.js
+++ increment.js
@@ -2,13 +2,13 @@ incrementAndShowValue();
 
 function incrementAndShowValue() {
   
-  var value = localStorage.getItem('visitcounter') || 0;
+  var value = localStorage.getItem('visitcounter') || 329;
   var newValue = ("00000" + (Number(value) + 1)).slice(-6);
   var container = document.getElementById("1");
   var counter = parseInt(newValue)
   localStorage.setItem("visitcounter", counter);
 
-  var sentence = "You are the " + counter + "th to enter this shortened link."
+  var sentence = "אתם האדם ה" + counter + "שנכנס לקישור הזה"
 
   setTimeout(function(){
   window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
