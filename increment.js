798c56d826a5a13915c6b78dfcddb007ba4540e3
ShaharYemini <76625369+ShaharYemini@users.noreply.github.com>
2021-12-05 15:03:57 +0200

    007

--- increment.js
+++ increment.js
@@ -1,14 +1,15 @@
 incrementAndShowValue();
 
 function incrementAndShowValue() {
-  
+  minutes = Math.round(Date.now() / (1000*60) )
+  minutesPast = localStorage.getItem('minutes') || minutes
   var value = localStorage.getItem('visitcounter') || 329;
   var newValue = ("00000" + (Number(value) + 1)).slice(-6);
   var container = document.getElementById("1");
-  var counter = parseInt(newValue)
+  var counter = parseInt(newValue) 
   localStorage.setItem("visitcounter", counter);
 
-  var sentence = "מזל טוב! \n אתה האדם ה" + counter + " להיכנס לקישור!"
+  var sentence = "מזל טוב! \n אתה האדם ה" + (counter + minutesPast - minutes) + " לשנכנס לקישור!"
 
   setTimeout(function(){
   window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
